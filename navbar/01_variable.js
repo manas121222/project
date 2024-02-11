2ebb286d823d9631b1e9949e437451c652980fee
Manas Bhardwaj <bhardwajmanas121@gmail.com>
2024-02-11 16:43:58 +0530

    01_variable.js

---
+++ navbar/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountemail="manas121@gmail.com"
+var accountpassward="12345"
+accountcity="jaipur"
+
+
+accountEmail ="manas1111@gmail.com"
+accountpassward= "12348"
+accountcity= "benguluru"
+
+
+console.log(accountId);
+console.table([accountId,accountpassward,acconutcity]);

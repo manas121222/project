9f22cafa0cc29f08d9578aa64a2f6593264221a7
Manas Bhardwaj <bhardwajmanas121@gmail.com>
2024-02-12 22:12:03 +0530

    operation

---
+++ navbar/04_operation.js
@@ -0,0 +1,16 @@
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+let str1 = "manas"
+let str2 =" good morning"
+let str3 = str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(+true);
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
+//study of frefix or porfix;

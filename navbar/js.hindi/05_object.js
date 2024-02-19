6f41eed37c6df0a660f8ef52e31e45ac94d835d8
Manas Bhardwaj <bhardwajmanas121@gmail.com>
2024-02-19 23:01:45 +0530

    05_object.js

---
+++ navbar/js.hindi/05_object.js
@@ -0,0 +1,15 @@
+ const course = {
+    coursemmae: "js in hindi",
+    Price: '9999',
+    courseInstructor: "manas"
+
+
+ }
+// course.courseinstructor
+const{courseInstructor : instructor} = course
+console.log(instructor);
+
+ //const navbar = ({company}) => {
+
+ //}
+// navbar(company = "maas"

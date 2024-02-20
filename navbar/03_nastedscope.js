204fa3dc908d2339f22d5fbffc4b5b3b3791c5dc
Manas Bhardwaj <bhardwajmanas121@gmail.com>
2024-02-20 21:36:18 +0530

    03_nastedscope.js

---
+++ navbar/03_nastedscope.js
@@ -0,0 +1,12 @@
+function one (){
+    const username = "hitesh"
+    function two (){
+        const website = "youtube"
+        console.log(username);
+    }
+    two()
+}
+//one()
+if (true){
+    const username = "hitesh"
+}

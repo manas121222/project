b0019cded3d31e868c580c4c840984c271b47015
Manas Bhardwaj <bhardwajmanas121@gmail.com>
2024-02-21 23:08:43 +0530

    06_iife.js

---
+++ navbar/06_iife.js
@@ -0,0 +1,8 @@
+// IIFE -> immadiately invoked function expression 
+ (function chai (){
+    console.log(`DB CONNECTED`)
+ })();
+ ((name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+ })(`manas`)
+ 

6bb26cf656d1c63b7e18e98d20b39c7a49bb3d74
Manas Bhardwaj <bhardwajmanas121@gmail.com>
2024-05-28 09:39:09 +0530

    manas bhardwaj

--- navbar/staack/01_stack.cpp
+++ navbar/staack/01_stack.cpp
@@ -1,59 +1,90 @@
-#include<iostream>
-#include <stack>
-using namespace std;
-class stack {
-    public :
-    int *arr;
-    int top;
-    int size;
-    // behavior
-    stack(int size) {
-    this -> size = size;
-    arr= new int[size];
-    top = -1;
-    }
-    void push(int element){
-    if (size-top >1){
-        top++;
-        arr[top] = element;
-    }
-    else {
-        cout << "stack overflow"<< endl;
-    }
-    }
-    void pop(){
-        if (top >=0){
-            tip--;
-        }
-        else{
-            cout << "stack underFlow" <<endl;
-        }
+// #include<iostream>
+// #include <stack>
+// using namespace std;
+// class stack {
+//     public :
+//     int *arr;
+//     int top;
+//     int size;
+//     // behavior
+//     stack(int size) {
+//     this -> size = size;
+//     arr= new int[size];
+//     top = -1;
+//     }
+//     void push(int element){
+//     if (size-top >1){
+//         top++;
+//         arr[top] = element;
+//     }
+//     else {
+//         cout << "stack overflow"<< endl;
+//     }
+//     }
+//     void pop(){
+//         if (top >=0){
+//             tip--;
+//         }
+//         else{
+//             cout << "stack underFlow" <<endl;
+//         }
 
-    }
-    int peek(){
-  if(top >= 0 ){
-  return arr[top];
-  }
-  else{
-    cout << "stack is empty"<<endl;
-    return -1;
-  }
-    }
-    bool  empty(){
-        if (top == -1){
-            return true;
-            else{
-                return false;
-            }
-        }
+//     }
+//     int peek(){
+//   if(top >= 0 ){
+//   return arr[top];
+//   }
+//   else{
+//     cout << "stack is empty"<<endl;
+//     return -1;
+//   }
+//     }
+//     bool  empty(){
+//         if (top == -1){
+//             return true;
+//             else{
+//                 return false;
+//             }
+//         }
 
-    }
-};
-int main(){
-    stack st(5)
-    st.puch(5);
-    st.puch(22);
-    st.puch(44);
-    st.puch(55);
-    cout << st.peek<<endl;
-}
+//     }
+// };
+// int main(){
+//     stack st(5)
+//     st.puch(5);
+//     st.puch(22);
+//     st.puch(44);
+//     st.puch(55);
+//     cout << st.peek<<endl;
+// }
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++)
+//     {
+//         int x;
+//         cin>>x;
+//         arr[i]=x;
+//     }
+// map<int,int>m;
+    // for(int i=0;i<n;i++)
+//    
+
+// #include<bits/stdc++.h>
+// using namespace std;
+//  int main()
+//  {
+//      int n;
+//     cin>>n;
+//     int arr[n];
+//      for(int i=0;i<n;i++)
+//      {
+//          int x;
+//          cin>>x;
+//          arr[i]=x;
+//      }
+
